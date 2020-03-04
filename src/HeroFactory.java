760c4c04e3517e9b200196bc1e47549b6a13967a
Jonah Garza <jgarza262@eagles.ewu.edu>
2020-03-04 14:57:10 -0800

    Simple Factory
    I added the HeroFactory and the MonsterFactory.
    I altered the chooseHero() and generateMonster() methods.

---
+++ src/HeroFactory.java
@@ -0,0 +1,17 @@
+
+public class HeroFactory {
+	public HeroFactory createHero(int choice) 
+	{
+		
+		switch(choice)
+		{
+			case 1: return new Warrior();
+
+			case 2: return new Sorceress();
+
+			case 3: return new Thief();
+		}//end switch
+		return null;
+	}
+
+}
---
+++ src/MonsterFactory.java
@@ -0,0 +1,15 @@
+
+public class MonsterFactory {
+	public Monster createMonster(int choice)
+	{
+		switch(choice)
+		{
+			case 1: return new Ogre();
+
+			case 2: return new Gremlin();
+
+			case 3: return new Skeleton();
+		}//end switch
+		return null;
+	}
+}
