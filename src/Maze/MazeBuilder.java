89d9dd9a65091b9c71b7fada8afb5ce00864d943
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2020-03-11 10:29:51 -0700

    LockBorder's Added
    Added a method to lock all the border door's

--- src/Maze/MazeBuilder.java
+++ src/Maze/MazeBuilder.java
@@ -22,12 +22,23 @@ public class MazeBuilder {
 		maze.setRooms(roomSetup);
 	}
 
+	//Needs Work
 	private void initiliazeDoors(Room[][] roomSetup) {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				//setup all of the doors
 			}
 		}
+		lockAllBorderDoors(roomSetup);
 	}
 
+	private void lockAllBorderDoors(Room[][] roomSetup) {
+		for(int i = 0; i < 5; i++) {
+			roomSetup[0][i].getNorth().close();//Top rooms
+			roomSetup[4][i].getSouth().close();//Bottom rooms
+			roomSetup[i][0].getEast().close();//LeftSide rooms
+			roomSetup[i][4].getWest().close();//RightSide rooms
+		}
+	}
+	
 }
