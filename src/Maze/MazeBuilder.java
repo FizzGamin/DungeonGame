3084c61a9a8f4e0fd8d6fdec93ff88bae17401ae
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2020-03-11 17:49:08 -0700

    Added Testing stuff
    Added stuff mainly for testing

--- src/Maze/MazeBuilder.java
+++ src/Maze/MazeBuilder.java
@@ -2,7 +2,28 @@ package Maze;
 
 public class MazeBuilder {
 	
-	public Maze buildMaze() {
+	public static void main(String[] args) {
+		Maze maze = buildMaze();
+		print(maze);
+		
+	}
+	
+	public static void print(Maze maze) {
+		Room[][] rooms = maze.getRooms();
+		System.out.println("*********");
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(rooms[i][j].getEast().isClosed())
+					System.out.print("*");
+				else
+					System.out.print("|");
+				System.out.print(rooms[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static Maze buildMaze() {
 		Maze maze = new Maze();
 		initiliazeMaze(maze);
 		
@@ -10,12 +31,13 @@ public class MazeBuilder {
 		return maze;
 	}
 
-	private void initiliazeMaze(Maze maze) {
+	private static void initiliazeMaze(Maze maze) {
 		Room[][] roomSetup = new Room[5][5];
 		
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				roomSetup[i][j] = new Room();
+				roomSetup[i][j].initializeRoom();
 			}
 		}
 		initiliazeDoors(roomSetup);
@@ -23,7 +45,7 @@ public class MazeBuilder {
 	}
 
 	//Needs Work
-	private void initiliazeDoors(Room[][] roomSetup) {
+	private static void initiliazeDoors(Room[][] roomSetup) {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				//setup all of the doors
@@ -32,7 +54,7 @@ public class MazeBuilder {
 		lockAllBorderDoors(roomSetup);
 	}
 
-	private void lockAllBorderDoors(Room[][] roomSetup) {
+	private static void lockAllBorderDoors(Room[][] roomSetup) {
 		for(int i = 0; i < 5; i++) {
 			roomSetup[0][i].getNorth().close();//Top rooms
 			roomSetup[4][i].getSouth().close();//Bottom rooms
