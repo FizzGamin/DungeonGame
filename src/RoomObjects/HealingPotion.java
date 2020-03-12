4d14035bb369ba87e4034565cc6993137464d9d0
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2020-03-11 19:18:15 -0700

    Added usePotion Method
    Added in use potion method and built contructor

--- src/RoomObjects/HealingPotion.java
+++ src/RoomObjects/HealingPotion.java
@@ -1,7 +1,22 @@
 package RoomObjects;
 
+import java.util.Random;
+
+import DungeonGame.Hero;
+
 public class HealingPotion implements RoomObject {
+	private Hero hero = Hero.getGameHero();
+	
+	
 	HealingPotion(){
-		
+		hero.setNumHealingPotions(hero.getNumHealingPotions() + 1);
+	}
+	
+	public void usePotion(){
+		Random ran = new Random();
+		int randomInt = ran.nextInt(11) + 5;
+		System.out.println(randomInt);
+		hero.changeHitPoints(randomInt);
+		hero.setNumHealingPotions(Hero.getGameHero().getNumHealingPotions() - 1);
 	}
 }
