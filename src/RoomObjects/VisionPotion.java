7aaa2b02cd5d5db9091f6455465544a64927492e
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2020-03-12 22:11:19 -0700

    Fixed usePotion

--- src/RoomObjects/VisionPotion.java
+++ src/RoomObjects/VisionPotion.java
@@ -5,49 +5,49 @@ import Maze.MazeBuilder;
 
 public class VisionPotion implements RoomObject {
 	
-	public void usePotion() {
+	public static void usePotion() {
 		Maze maze = MazeBuilder.getMaze();
 		int row = maze.getPlayerPositionRow();
 		int col = maze.getPlayerPositionCol();
 		//If no where near a wall
 		if(row > 0 && col > 0 && row < 4 && col < 4) {
-			for(int i = row-1; i < row+1; i++) {
+			for(int i = row-1; i < row+2; i++) {
 				MazeBuilder.printRow(i, col-1, col+1, maze.getRooms());
 			}
 		}
 		//If on top row
-		else if(row == 0 && col > 0) {
-			for(int i = row; i < row+1; i++) {
+		else if(row == 0 && col > 0 && col < 4) {
+			for(int i = row; i < row+2; i++) {
 				MazeBuilder.printRow(i, col-1, col+1, maze.getRooms());
 			}
 		}
 		//If on bottom row
-		else if(row == 4 && col > 0) {
+		else if(row == 4 && col > 0 && col < 4) {
 			for(int i = row-1; i < row; i++) {
 				MazeBuilder.printRow(i, col-1, col+1, maze.getRooms());
 			}
 		}
 		//If on left side wall
-		else if(col == 0 && row > 0) {
-			for(int i = row-1; i < row+1; i++) {
+		else if(col == 0 && row > 0 && row < 4) {
+			for(int i = row-1; i < row+2; i++) {
 				MazeBuilder.printRow(i, col, col+1, maze.getRooms());
 			}
 		}
 		//If on right side wall
-		else if(col == 4 && row > 0) {
-			for(int i = row-1; i < row+1; i++) {
+		else if(col == 4 && row > 0 && row < 4) {
+			for(int i = row-1; i < row+2; i++) {
 				MazeBuilder.printRow(i, col-1, col, maze.getRooms());
 			}
 		}
 		//If in top left corner
 		else if(row == 0 && col == 0){
-			for(int i = row; i < row+1; i++) {
+			for(int i = row; i < row+2; i++) {
 				MazeBuilder.printRow(i, col, col+1, maze.getRooms());
 			}
 		}
 		//If in top right corner
 		else if(row == 0 && col == 4){
-			for(int i = row; i < row+1; i++) {
+			for(int i = row; i < row+2; i++) {
 				MazeBuilder.printRow(i, col-1, col, maze.getRooms());
 			}
 		}
